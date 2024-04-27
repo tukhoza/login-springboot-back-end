1a7bfe85ae674b3743dfb58530e6f66b00e0c401
MID-3WDPXY2\BigBoss <tumelokhoza3@gmail.com>
2024-04-27 12:31:09 +0200

    Spring boot application

---
+++ src/test/java/com/app/login/LoginAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
